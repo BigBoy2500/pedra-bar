4f90d33e550d0a57783c42ae3a7a359f47307056
BigBoy2500 <migassilva25@gmail.com>
2025-06-10 01:23:51 +0100

    Menu changes

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+{/*import { useState } from 'react'
 import MenuLineItem from '@/components/MenuLineItem'
 import { menuData } from '@/data/menuData'
 
@@ -25,10 +25,10 @@ export default function Menu() {
         <div className="container">
           <h1 className="text-4xl font-bold text-primary text-center mb-12">
             Nosso Menu
-          </h1>
+          </h1>*/}
 
           {/* Category Filters */}
-          <div className="flex justify-center space-x-4 mb-12">
+          {/*<div className="flex justify-center space-x-4 mb-12">
             {categories.map((cat) => (
               <button
                 key={cat}
@@ -38,15 +38,15 @@ export default function Menu() {
                 {menuData[cat].label}
               </button>
             ))}
-          </div>
+          </div>*/}
 
           {/* Título da categoria */}
-          <h2 className="text-2xl font-serif font-bold text-primary mb-6 text-center">
+          {/*<h2 className="text-2xl font-serif font-bold text-primary mb-6 text-center">
             {menuData[activeCategory].label}
-          </h2>
+          </h2>*/}
 
           {/* Menu Items List */}
-          <div className="bg-gray-100 border border-green-700 rounded-md px-4 md:px-8 py-6 md:py-10">
+          {/*<div className="bg-gray-100 border border-green-700 rounded-md px-4 md:px-8 py-6 md:py-10">
             {menuData[activeCategory].items.map((item, index) => (
               <MenuLineItem key={index} {...item} />
             ))}
@@ -55,4 +55,19 @@ export default function Menu() {
       </div>
     </main>
   )
-} 
+} */}
+
+
+export default function Menu() {
+  return (
+    <main>
+      <div className="pt-32 pb-20">
+        <div className="container text-center">
+          <h1 className="text-4xl font-bold text-primary mb-12 mt-12">
+            Em breve disponível...
+          </h1>
+        </div>
+      </div>
+    </main>
+  )
+}
